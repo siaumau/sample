5e1957bb1fcbd42b06315712da37762f6326538b
eric.lai <eric.lai@xsgames.com>
2021-04-09 09:34:03 +0800

    [modify]測試有無redis功能

--- redis.php
+++ redis.php
@@ -1,8 +1,4 @@
 <?php
     //连接本地的 Redis 服务
    $redis = new Redis();
-   $redis->connect('127.0.0.1', 6379);
-   echo "Connection to server successfully";
-         //查看服务是否运行
-   echo "Server is running: " . $redis->ping();
 ?>
